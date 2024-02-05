122356aaf726a680618e5d969f77011399f96262
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-05 19:57:01 +0300

    Create 04-IdentityMatrix.py

---
+++ 04-IdentityMatrix.py
@@ -0,0 +1,14 @@
+# identity matrix
+
+identity_matrix = np.identity(5)
+
+print(identity_matrix)
+
+"""
+[[1. 0. 0. 0. 0.]
+ [0. 1. 0. 0. 0.]
+ [0. 0. 1. 0. 0.]
+ [0. 0. 0. 1. 0.]
+ [0. 0. 0. 0. 1.]]
+
+"""
