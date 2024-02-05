f04eee04345019ac30b1bdafcf7160dfc03393dd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-05 19:49:58 +0300

    Create 01-CreateAMatrix.py

---
+++ 01-CreateAMatrix.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+#create a matrix
+
+matrix_A = np.array([[3, 2.5, 0, 1], [2.7, 4, 1, 21], [2.6, 0, 11, 3]])
+
+print(f"Our matrix:\n\n {matrix_A}")
+
+"""
+Our matrix:
+
+ [[ 3.   2.5  0.   1. ]
+ [ 2.7  4.   1.  21. ]
+ [ 2.6  0.  11.   3. ]]
+
+"""
