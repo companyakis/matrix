3594238792c0edefd2397484319f19069f851c13
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-05 19:57:40 +0300

    Create 05-Zeros.py

---
+++ 05-Zeros.py
@@ -0,0 +1,16 @@
+# zeros
+
+zeros_matrix = np.zeros(4)
+
+print(zeros_matrix) # [0. 0. 0. 0.]
+
+zeros_matrix_2 = np.zeros([3, 3])
+
+print(zeros_matrix_2)
+
+"""
+[[0. 0. 0.]
+ [0. 0. 0.]
+ [0. 0. 0.]]
+
+"""
