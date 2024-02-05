f5f42e7e28946c144526e8974957a47c19b88378
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-05 19:51:35 +0300

    Create 03-Reshaping.py

---
+++ 03-Reshaping.py
@@ -0,0 +1,15 @@
+#reshaping
+
+matrix_b = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+new_matrix_b = matrix_b.reshape(5, 2)
+
+print(new_matrix_b)
+
+"""
+[[ 1  2]
+ [ 3  4]
+ [ 5  6]
+ [ 7  8]
+ [ 9 10]]
+"""
