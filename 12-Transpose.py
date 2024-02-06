47cecc4e2dc70ed671d5e7a158d9d01eb66710e2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-07 01:42:29 +0300

    Create 12-Transpose.py

---
+++ 12-Transpose.py
@@ -0,0 +1,14 @@
+# transpose
+
+matrix_1 = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]) # 3 * 4
+
+matrix_1_T = np.transpose(matrix_1) 
+
+print(matrix_1_T)
+
+"""
+[[ 1  5  9]
+ [ 2  6 10]
+ [ 3  7 11]
+ [ 4  8 12]]
+"""
