aa5c1adca6c1bc842666305d0ffe05bba4ffdc79
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-07 01:11:36 +0300

    Create 10-AdditionOrSubtraction.py

---
+++ 10-AdditionOrSubtraction.py
@@ -0,0 +1,21 @@
+# addition or subtraction
+
+# matrix_1 + matrix_2 or matrix_1 - matrix_2
+
+matrix_1 = np.array([[4, 22], [-10, 45.78]])
+
+matrix_2 = np.array([[24, -30], [0.5, 3]])
+
+print(matrix_1 + matrix_2)
+
+print("*****************")
+    
+print(matrix_1 - matrix_2)
+
+"""
+[[28.   -8.  ]
+ [-9.5  48.78]]
+*****************
+[[-20.    52.  ]
+ [-10.5   42.78]]
+"""
